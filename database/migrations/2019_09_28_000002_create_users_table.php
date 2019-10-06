554eedab6d65d4e37b0f8c49bf26aced120bd145
Carl Vuorinen <carl.vuorinen@gmail.com>
2019-10-06 23:03:15 +0700

    Buat database awal

--- database/migrations/2019_09_28_000002_create_users_table.php
+++ database/migrations/2019_09_28_000002_create_users_table.php
@@ -10,22 +10,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-
             $table->string('name');
-
             $table->string('email');
-
             $table->datetime('email_verified_at')->nullable();
-
             $table->string('password');
-
             $table->string('remember_token')->nullable();
-
             $table->timestamps();
-
             $table->softDeletes();
-
         });
-
     }
 }
